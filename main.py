4575c07fbb7f9285cf271ba7418522c0503f90f7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-04 09:22:50 +0000

    Deploy whitebird/openvpn-dev-routes to github.com/whitebird/openvpn-dev-routes.git:gh-pages

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ import ipaddress
 OUTPUT = ""
 
 
+class DataFetchError(Exception):
+    """Exception raised for errors while fetching data.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        pass
+
 def transform_address(address):
     address_with_netmask = ipaddress.ip_network(
         address).with_netmask.split('/')
@@ -17,56 +27,76 @@ def transform_address(address):
 
 
 def add_amazon():
-    r = requests.get("https://ip-ranges.amazonaws.com/ip-ranges.json")
-    prefixes = r.json()["prefixes"]
-    prefixes.sort(key=lambda x: x["region"], reverse=True)
-    current_region = ""
-    output = "\n### Amazon EC2\n"
-    for prefix in prefixes:
-        if current_region != prefix["region"]:
-            output += "# " + prefix["region"] + "\n"
-            current_region = prefix["region"]
-        output += transform_address(prefix["ip_prefix"])
+    try:
+        print("Fetching Amazon EC2")
+        r = requests.get("https://ip-ranges.amazonaws.com/ip-ranges.json")
+        prefixes = r.json()["prefixes"]
+        prefixes.sort(key=lambda x: x["region"], reverse=True)
+        current_region = ""
+        output = "\n### Amazon EC2\n"
+        for prefix in prefixes:
+            if current_region != prefix["region"]:
+                output += "# " + prefix["region"] + "\n"
+                current_region = prefix["region"]
+            output += transform_address(prefix["ip_prefix"])
+    except:
+        raise DataFetchError("Error fetching data from Amazon EC2")
     return output
 
 
 def add_github():
-    output = "\n### GitHub\n"
-    r = requests.get("https://api.github.com/meta")
-    json = {k: v for k, v in r.json().items() if isinstance(v, list)}
-    current_group = ""
-    for group, address_list in json.items():
-        if current_group != group:
-            #output += transform_address(address)
-            output += "# " + group + "\n"
-            current_group = group
-        for address in address_list:
-            output += transform_address(address)
+    try:
+        print("Fetching GitHub")
+        output = "\n### GitHub\n"
+        r = requests.get("https://api.github.com/meta")
+        json = {k: v for k, v in r.json().items() if isinstance(v, list)}
+
+        if not json.items():
+            raise Exception
+
+        current_group = ""
+        for group, address_list in json.items():
+            if current_group != group:
+                #output += transform_address(address)
+                output += "# " + group + "\n"
+                current_group = group
+            for address in address_list:
+                output += transform_address(address)
+    except:
+        raise DataFetchError("Error fetching data from GitHub")
     return output
 
 
 def add_gitlab():
+    print("Fetching GitLab")
     # Placeholder for when public api is released
     # See https://gitlab.com/gitlab-com/infrastructure/issues/434
     return "\n## GitLab\nroute 35.231.145.151 255.0.0.0\n"
 
-def add_atlassian():
-    output = "\n### Atlassian\n"
-    r = requests.get("https://ip-ranges.atlassian.com/")
-    for item in r.json()["items"]:
-        output += "route " + item["network"] + " " + item["mask"] + "\n"
-    return output
-    
 
-OUTPUT += add_amazon()
-OUTPUT += add_github()
-OUTPUT += add_gitlab()
-OUTPUT += add_atlassian()
+def add_atlassian():
+    try:
+        print("Fetching Atlassian")
+        output = "\n### Atlassian\n"
+        r = requests.get("https://ip-ranges.atlassian.com/")
+        for item in r.json()["items"]:
+            output += "route " + item["network"] + " " + item["mask"] + "\n"
+    except:
+        raise DataFetchError("Error fetching data from Amazon Atlassian")
+    return output
+
 
 try:
+    OUTPUT += add_amazon()
+    OUTPUT += add_github()
+    OUTPUT += add_gitlab()
+    OUTPUT += add_atlassian()
     with open("routes.txt", 'w') as out:
         out.write(OUTPUT)
     print("Output written to 'routes.txt'")
 except IOError:
     print("Error while writing 'routes.txt'")
+    sys.exit(1)
+except Exception as e:
+    print(e)
     sys.exit(1)
